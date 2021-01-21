94565c7f3bc8905d124188102003c2f12ff540f6
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:18:53 -0600

    Initial commit

---
+++ doUnique/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblRandom = New System.Windows.Forms.Label()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.lblNum3 = New System.Windows.Forms.Label()
+        Me.btnMix = New System.Windows.Forms.Button()
+        Me.btnRange = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblRandom
+        '
+        Me.lblRandom.AutoSize = True
+        Me.lblRandom.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRandom.Location = New System.Drawing.Point(12, 9)
+        Me.lblRandom.Name = "lblRandom"
+        Me.lblRandom.Size = New System.Drawing.Size(159, 20)
+        Me.lblRandom.TabIndex = 1
+        Me.lblRandom.Text = "Random From 1 To 6"
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1.Location = New System.Drawing.Point(36, 196)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(51, 20)
+        Me.lblNum1.TabIndex = 2
+        Me.lblNum1.Text = "Num1"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum2.Location = New System.Drawing.Point(149, 196)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(51, 20)
+        Me.lblNum2.TabIndex = 3
+        Me.lblNum2.Text = "Num2"
+        '
+        'lblNum3
+        '
+        Me.lblNum3.AutoSize = True
+        Me.lblNum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum3.Location = New System.Drawing.Point(262, 196)
+        Me.lblNum3.Name = "lblNum3"
+        Me.lblNum3.Size = New System.Drawing.Size(51, 20)
+        Me.lblNum3.TabIndex = 4
+        Me.lblNum3.Text = "Num3"
+        '
+        'btnMix
+        '
+        Me.btnMix.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMix.Location = New System.Drawing.Point(12, 322)
+        Me.btnMix.Name = "btnMix"
+        Me.btnMix.Size = New System.Drawing.Size(325, 36)
+        Me.btnMix.TabIndex = 5
+        Me.btnMix.Text = "Mix"
+        Me.btnMix.UseVisualStyleBackColor = True
+        '
+        'btnRange
+        '
+        Me.btnRange.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRange.Location = New System.Drawing.Point(12, 364)
+        Me.btnRange.Name = "btnRange"
+        Me.btnRange.Size = New System.Drawing.Size(325, 36)
+        Me.btnRange.TabIndex = 6
+        Me.btnRange.Text = "Range"
+        Me.btnRange.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 412)
+        Me.Controls.Add(Me.btnRange)
+        Me.Controls.Add(Me.btnMix)
+        Me.Controls.Add(Me.lblNum3)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.lblRandom)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblRandom As System.Windows.Forms.Label
+    Friend WithEvents lblNum1 As System.Windows.Forms.Label
+    Friend WithEvents lblNum2 As System.Windows.Forms.Label
+    Friend WithEvents lblNum3 As System.Windows.Forms.Label
+    Friend WithEvents btnMix As System.Windows.Forms.Button
+    Friend WithEvents btnRange As System.Windows.Forms.Button
+
+End Class
---
+++ doUnique/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim intMax As Integer = 6
+    Dim intMin As Integer = 1
+
+    Private Sub btnMix_Click(sender As Object, e As EventArgs) Handles btnMix.Click
+        Randomize()
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intNum3 As Integer
+        Dim intTries As Integer = 0
+
+        Do
+            intNum1 = Int(Rnd() * intMax) + 1
+            intNum2 = Int(Rnd() * intMax) + 1
+            intNum3 = Int(Rnd() * intMax) + 1
+            intTries = intTries + 1
+        Loop While (intNum1 = intNum2 Or intNum1 = intNum3 Or intNum2 = intNum1 Or intNum2 = intNum3 Or intNum3 = intNum1 Or intNum3 = intNum2)
+        'MessageBox.Show("Tries: " & intTries)
+
+        lblNum1.Text = intNum1
+        lblNum2.Text = intNum2
+        lblNum3.Text = intNum3
+
+    End Sub
+
+    Private Sub btnRange_Click(sender As Object, e As EventArgs) Handles btnRange.Click
+        Const LIMIT As Integer = 100
+
+        Do
+            intMax = InputBox("Enter The Max")
+            If (intMax > LIMIT) Then
+                MessageBox.Show("ERROR! Enter a number 100 or less")
+            End If
+        Loop While (intMax > LIMIT)
+
+        lblRandom.Text = "Random From 1 To " & intMax
+    End Sub
+End Class
